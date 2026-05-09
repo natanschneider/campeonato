3c411e4d881ab71949e555561428b92e4cb2fcb4
Natan Schneider <natan.schneider@protonmail.com>
2026-05-09 18:43:22 -0300

    *feat* add error pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,25 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import ErrorComponent from "@/pages/ErrorComponent";
+import NotFound from "@/pages/NotFound";
 
 export function getRouter() {
-  const router = createTanStackRouter({
-    routeTree,
-    scrollRestoration: true,
-    defaultPreload: 'intent',
-    defaultPreloadStaleTime: 0,
-  })
+	const router = createTanStackRouter({
+		defaultNotFoundComponent: () => <NotFound />,
+		defaultErrorComponent: ({ error, reset }) => (
+			<ErrorComponent error={error} reset={reset} />
+		),
+		routeTree,
+		scrollRestoration: true,
+		defaultPreload: "intent",
+		defaultPreloadStaleTime: 0,
+	});
 
-  return router
+	return router;
 }
 
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: ReturnType<typeof getRouter>
-  }
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: ReturnType<typeof getRouter>;
+	}
 }
