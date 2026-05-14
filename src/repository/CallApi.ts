98d596787c1084f927bd7e5d82362e11e57ee011
Natan Schneider <natan.schneider@deltasul.com.br>
2026-05-14 14:47:57 -0300

    *fix* call api only on server

--- src/repository/CallApi.ts
+++ src/repository/CallApi.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import { createServerFn } from '@tanstack/react-start';
 
-export default async function CallApi() {
+const CallApi = createServerFn({ method: 'GET', strict: false }).handler(async () => {
     const response = await axios.get('http://jsuol.com.br/c/monaco/utils/gestor/commons.js?&file=commons.uol.com.br/sistemas/esporte/modalidades/futebol/campeonatos/dados/2025/30/dados.json');
 
     return response.data;
-}
+});
+
+export default CallApi;
